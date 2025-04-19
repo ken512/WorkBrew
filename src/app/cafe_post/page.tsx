7a1677be4d602935cd1e8355c947520ef4770b60
porota <ken887265@gmail.com>
2025-04-19 13:05:39 +0900

    commit

--- src/app/cafe_post/page.tsx
+++ src/app/cafe_post/page.tsx
@@ -2,10 +2,17 @@ import React, { Suspense } from "react";
 import { HeaderPublic } from "../_components/HeaderPublic";
 import { FooterDefault } from "../_components/Footer/FooterDefault";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import CafePostClient from "./CafePostClient";
 =======
 import CafePostClient from "./cafePostClient";
 >>>>>>> 2750038 (commit)
+=======
+import CafePostClient from "./cafePostClient";
+=======
+import CafePostClient from "./CafePostClient";
+>>>>>>> 087321a (commit)
+>>>>>>> 0a76d59 (commit)
 
 const CafePost: React.FC = () => {
   return (
