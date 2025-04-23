948c9725bc57bb67ac922600c8d606d834c56b3e
ken512 <ken887265@gmail.com>
2025-04-23 18:34:34 +0900

    commit

--- src/app/_components/FaqItem.tsx
+++ src/app/_components/FaqItem.tsx
@@ -9,21 +9,25 @@ type FaqItemProps = {
 
 export const FaqItem: React.FC<FaqItemProps> = ({ question, answer }) => {
   return (
-    <div className="mb-10 font-bold sm:text-sm w-full max-w-3xl sm:max-w-[350px]">
-      <div className="flex items-start mb-6">
+    <div className="mb-16  w-full max-w-3xl sm:max-w-[350px] text-black">
+      {/* Q */}
+      <div className="flex items-start gap-3 mb-4">
         <span className="bg-blue-500 text-white font-bold rounded-full w-6 h-6 min-w-[1.5rem] flex items-center justify-center mr-2">
           Q
         </span>
-        <p className="text-lg font-semibold">{question}</p>
+        <p className="text-lg font-bold leading-snug">{question}</p>
       </div>
-      <div className="flex items-start">
+
+      {/* A */}
+      <div className="flex items-start gap-3">
         <span className="bg-red-400 text-white font-bold rounded-full w-6 h-6 min-w-[1.5rem] flex items-center justify-center mr-2">
           A
         </span>
-        <p className="text-lg sm:text-sm">{answer}</p>
+        <div className="text-sm font-normal whitespace-pre-line leading-relaxed" dangerouslySetInnerHTML={{ __html: answer }}/>
       </div>
-      <div className="border-b border-black my-16" />
-      
+
+      {/* 区切り線 */}
+      <div className="border-b border-gray-400 mt-8" />
     </div>
   );
 };
