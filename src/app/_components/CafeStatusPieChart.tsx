f52850f2bde7998b58c0695f625cca6aab1b4464
porota <ken887265@gmail.com>
2025-04-24 23:38:56 +0900

    commit

--- src/app/_components/CafeStatusPieChart.tsx
+++ src/app/_components/CafeStatusPieChart.tsx
@@ -11,6 +11,17 @@ import {
 import { PieChartData } from "../_types/PieChartProps";
 const COLORS = ["#00C49F", "#FFBB28", "#FF4444"]; // 緑・オレンジ・赤など
 
+type ChartData = {
+  name: string;
+  percentage: number;
+  cx: number;
+  cy: number;
+  midAngle: number;
+  outerRadius: number;
+  innerRadius: number;
+  index: number;
+};
+
 export const CafeStatusPieChart: React.FC<{ chartData: PieChartData }> = ({
   chartData,
 }) => {
@@ -34,11 +45,40 @@ export const CafeStatusPieChart: React.FC<{ chartData: PieChartData }> = ({
               <Pie
                 dataKey="count"
                 data={data}
-                cx="35%"
+                cx="55%"
                 cy="50%"
-                outerRadius={80}
+                outerRadius={70}
                 fill="#8884d8"
-                label={({ name, percentage }) => `${name} (${percentage}%)`}
+                labelLine={false}
+                label={({
+                  cx,
+                  cy,
+                  midAngle,
+                  outerRadius,
+                  name,
+                  percentage,
+                  index,
+                  innerRadius
+                }: ChartData) => {
+                  if (percentage === 0) return null;
+                  const RADIAN = Math.PI / 180;
+                  const radius = innerRadius + (outerRadius - innerRadius) * 1.3;
+                  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+                  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+                  return (
+                    <text
+                    x={x < cx ? x + 20 : x - 10}
+                    y={y < cy ? y : y + 10}
+                      textAnchor={x > cx ? "start" : "end"}
+                      dominantBaseline="central"
+                      fontSize={12}
+                      fill={COLORS[index % COLORS.length]}
+                    >
+                      {`${name} (${percentage}%)`}
+                    </text>
+                  );
+                }}
               >
                 {data.map((_, index) => (
                   <Cell
