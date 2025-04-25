143a315bae92e15b132aa68c92270f256d893537
ken512 <ken887265@gmail.com>
2025-04-25 16:12:58 +0900

    commit

--- src/app/api/admin/user_account/route.ts
+++ src/app/api/admin/user_account/route.ts
@@ -41,13 +41,6 @@ export const GET = async (request: NextRequest) => {
             },
           },
         },
-        information: {
-          select: {
-            latestInfo: true,
-            recommended_cafe: true,
-            recommendation_reason: true,
-          },
-        },
       },
     });
 
