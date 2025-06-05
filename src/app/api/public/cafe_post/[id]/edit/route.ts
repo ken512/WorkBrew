ea00571e047b50683290fa798e1036d4cca65706
porota <ken887265@gmail.com>
2025-06-05 23:17:05 +0900

    commit

--- src/app/api/public/cafe_post/[id]/edit/route.ts
+++ src/app/api/public/cafe_post/[id]/edit/route.ts
@@ -1,26 +1,28 @@
 import { getCurrentUser } from "@/_utils/supabase";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-
+import { UpdateStatus } from "@/app/_types/UpdateStatus";
 const prisma = new PrismaClient();
 
-export const GET = async (request: NextRequest, {params}: {params: {id: string}}) => {
-
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
   try {
-    const { currentUser, error} = await getCurrentUser(request);
+    const { currentUser, error } = await getCurrentUser(request);
 
-    if(error || !currentUser) {
-      return NextResponse.json({message: "Unauthorized"}, { status: 400});
+    if (error || !currentUser) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 400 });
     }
-    const {id} = params;
+    const { id } = params;
     const cafes = await prisma.cafe.findUnique({
-      where: {id: parseInt(id), 
-        users: {supabaseUserId: currentUser.user.id,
-        },
+      where: {
+        id: parseInt(id),
+        users: { supabaseUserId: currentUser.user.id },
       },
       include: {
         users: {
-          select: {id: true, userName: true, profileIcon: true},
+          select: { id: true, userName: true, profileIcon: true },
         },
         favorites: {
           select: {
@@ -30,9 +32,25 @@ export const GET = async (request: NextRequest, {params}: {params: {id: string}}
         },
       },
     });
-    return NextResponse.json({status: "OK", cafes: cafes}, {status: 200});
+    return NextResponse.json({ status: "OK", cafes: cafes }, { status: 200 });
   } catch (error) {
-    if(error instanceof Error)
-      return NextResponse.json({status: error.message}, {status: 400});
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 });
   }
-}
+};
+
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const {currentUser, error} = await getCurrentUser(request);
+  if(error || !currentUser) {
+    return NextResponse.json({status: 400});
+  }
+
+  try {
+    const {id} = params;
+
+    const updateData:UpdateStatus = {};
+  }
+};
