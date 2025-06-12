c3be148b38b896d531a1b79dedc2be52a837fe44
ken512 <ken887265@gmail.com>
2025-06-12 22:17:16 +0900

    commit

--- src/app/admin/_components/CafePostEditor.tsx
+++ src/app/admin/_components/CafePostEditor.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
 import { Input } from "@/app/_components/Input";
 import { CafeFormFields } from "../_data/cafeFormFields";
 import { TextArea } from "@/app/_components/TextArea";
@@ -9,7 +10,6 @@ import api from "@/_utils/api";
 import toast, { Toaster } from "react-hot-toast";
 import "../../globals.css";
 
-
 export const CafePostEditor: React.FC<CafeFormStateReturn> = ({
   formState,
   setFormState,
@@ -18,6 +18,16 @@ export const CafePostEditor: React.FC<CafeFormStateReturn> = ({
 }) => {
   const [clearSignal, setClearSignal] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const { id } = useParams();
+  const fetcher = (url: string) => api.get(url);
 
+  const {
+    data = { cafes: {} },
+    error,
+    isLoading,
+  } = useSWR(`/api/public/cafe_post/${id}/edit`, fetcher);
+  const cafe = data.cafes;
+  
+  
 
-}
+};
