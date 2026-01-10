e5df6208d7068235032f71b14f506a6af83ec1f2
山内建人 <yamauchikento@yamauchitakerunoMacBook-Pro.local>
2026-01-10 23:32:11 +0900

    refactor: CafeLocationFormStateをフォーム反映用の型に変更

--- src/app/admin/cafe_submission_form/types/cafeLocationFormState.ts
+++ src/app/admin/cafe_submission_form/types/cafeLocationFormState.ts
@@ -1,10 +1,9 @@
-import { Coordinate } from "./coordinate"
-import {  Cafe } from "@prisma/client";
+import { Cafe } from "@prisma/client";
+import { Coordinate } from "./coordinate";
+import { CafeFormStateProps } from "../../_types/CafeFormStateProps";
 
-// 現在地とお店の位置情報
-export type CafeLocationFormState = {
-  location?: Coordinate;
-  storeAddress?: Cafe["storeAddress"];
-  cafeName?: Cafe["cafeName"];
-  isLoading: boolean;
-};
+//現在地とお店の位置情報のレスポンスの結果をフォーム(投稿データ)に変換するためのフォームの型
+export type CafeLocationFormState = Pick<
+  CafeFormStateProps,
+  "cafeName" | "storeAddress"
+> & { placeId?: Cafe["placeId"]; locationCoordinates: Coordinate };
---
+++ src/app/admin/cafe_submission_form/types/placeCandidate.ts
@@ -0,0 +1,11 @@
+import { Cafe } from "@prisma/client";
+import { Coordinate } from "./coordinate";
+// 現在地とお店の位置情報のレスポンス型
+export type PlaceCandidate = {
+  placeId?: Cafe["placeId"];
+  cafeName: Cafe["cafeName"];
+  storeAddress: Cafe["storeAddress"];
+  locationCoordinates: Coordinate;
+  primaryType?: Cafe["primaryType"];
+  types: Cafe[];
+};
