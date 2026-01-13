8a1daa2f7d31f668f7c9e8527628a3be4e0ea30a
ken512 <ken887265@gmail.com>
2026-01-13 21:21:53 +0900

    refactor: Cafe関連の型定義からPlaceCandidate依存を削除

--- src/app/_types/Cafe.ts
+++ src/app/_types/Cafe.ts
@@ -1,14 +1,13 @@
 import { WifiSpeed,  WifiStability, SeatAvailability } from "@prisma/client";
-import { PlaceCandidate } from "../admin/cafe_submission_form/types/placeCandidate";
 
 export type Cafe = {
   id: number;
   cafeName: string;
   area: string;
   storeAddress: string;
-  placeId?: PlaceCandidate["placeId"];
-  primaryType?: PlaceCandidate["primaryType"];
-  types: PlaceCandidate["types"];
+  placeId?: string;
+  primaryType?: string;
+  types: string[];
   openingTime?: string;
   closingHours?: string;
   businessHours?: string;
--- src/app/admin/cafe_submission_form/types/placeCandidate.ts
+++ src/app/admin/cafe_submission_form/types/placeCandidate.ts
@@ -1,11 +1,10 @@
-import { Cafe } from "@prisma/client";
 import { Coordinate } from "./coordinate";
 // 現在地とお店の位置情報のレスポンス型
 export type PlaceCandidate = {
-  placeId?: Cafe["placeId"];
-  cafeName: Cafe["cafeName"];
-  storeAddress: Cafe["storeAddress"];
+  placeId?: string;
+  cafeName: string;
+  storeAddress: string;
   locationCoordinates: Coordinate;
-  primaryType?: Cafe["primaryType"];
-  types: Cafe["types"];
+  primaryType?: string;
+  types: string[];
 };
